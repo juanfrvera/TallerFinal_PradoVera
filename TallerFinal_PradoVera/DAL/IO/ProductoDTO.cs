269efbc3fcbc69061a4ccb08f7bece89dd54d05c
Lucas Martín Prado <Lucas-M-Prado@hotmail.com>
2018-12-27 21:10:46 -0300

    Carpeta IO agregada

---
+++ TallerFinal_PradoVera/DAL/IO/ProductoDTO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Autoservicio.IO
+{
+    internal struct ClienteDTO
+    {
+        private String numero;
+
+        public String Numero
+        {
+            get { return numero; }
+            set { numero = value; }
+        }
+
+        private String nombre;
+
+        public String Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        private String tipo;
+
+        public String Tipo
+        {
+            get { return tipo; }
+            set { tipo = value; }
+        }
+    }
+}
+
