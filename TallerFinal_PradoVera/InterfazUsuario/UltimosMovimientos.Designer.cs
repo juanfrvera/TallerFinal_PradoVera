08a3656423a48c477c7040783b6fc54cb5684d06
Juan Vera <juanfrvera@gmail.com>
2020-12-08 17:01:04 -0300

    fix: título de últimos movimientos

--- TallerFinal_PradoVera/InterfazUsuario/UltimosMovimientos.Designer.cs
+++ TallerFinal_PradoVera/InterfazUsuario/UltimosMovimientos.Designer.cs
@@ -28,57 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.columnFecha = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnMonto = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.buttonVolver = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // listView1
-            // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+         this.listView1 = new System.Windows.Forms.ListView();
+         this.columnFecha = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+         this.columnMonto = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+         this.buttonVolver = new System.Windows.Forms.Button();
+         this.SuspendLayout();
+         // 
+         // listView1
+         // 
+         this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnFecha,
             this.columnMonto});
-            this.listView1.Location = new System.Drawing.Point(17, 58);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(550, 290);
-            this.listView1.TabIndex = 0;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
-            // 
-            // columnFecha
-            // 
-            this.columnFecha.Text = "Fecha";
-            this.columnFecha.Width = 127;
-            // 
-            // columnMonto
-            // 
-            this.columnMonto.Text = "Monto";
-            this.columnMonto.Width = 219;
-            // 
-            // buttonVolver
-            // 
-            this.buttonVolver.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonVolver.Location = new System.Drawing.Point(137, 375);
-            this.buttonVolver.Name = "buttonVolver";
-            this.buttonVolver.Size = new System.Drawing.Size(300, 50);
-            this.buttonVolver.TabIndex = 1;
-            this.buttonVolver.Text = "Volver";
-            this.buttonVolver.UseVisualStyleBackColor = true;
-            this.buttonVolver.Click += new System.EventHandler(this.buttonVolver_Click);
-            // 
-            // UltimosMovimientos
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 450);
-            this.ControlBox = false;
-            this.Controls.Add(this.buttonVolver);
-            this.Controls.Add(this.listView1);
-            this.Name = "UltimosMovimientos";
-            this.Text = "UltimosMovimientos";
-            this.Click += new System.EventHandler(this.ClickEnVentana);
-            this.ResumeLayout(false);
+         this.listView1.HideSelection = false;
+         this.listView1.Location = new System.Drawing.Point(17, 58);
+         this.listView1.Name = "listView1";
+         this.listView1.Size = new System.Drawing.Size(550, 290);
+         this.listView1.TabIndex = 0;
+         this.listView1.UseCompatibleStateImageBehavior = false;
+         this.listView1.View = System.Windows.Forms.View.Details;
+         // 
+         // columnFecha
+         // 
+         this.columnFecha.Text = "Fecha";
+         this.columnFecha.Width = 127;
+         // 
+         // columnMonto
+         // 
+         this.columnMonto.Text = "Monto";
+         this.columnMonto.Width = 219;
+         // 
+         // buttonVolver
+         // 
+         this.buttonVolver.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.buttonVolver.Location = new System.Drawing.Point(137, 375);
+         this.buttonVolver.Name = "buttonVolver";
+         this.buttonVolver.Size = new System.Drawing.Size(300, 50);
+         this.buttonVolver.TabIndex = 1;
+         this.buttonVolver.Text = "Volver";
+         this.buttonVolver.UseVisualStyleBackColor = true;
+         this.buttonVolver.Click += new System.EventHandler(this.buttonVolver_Click);
+         // 
+         // UltimosMovimientos
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(584, 450);
+         this.ControlBox = false;
+         this.Controls.Add(this.buttonVolver);
+         this.Controls.Add(this.listView1);
+         this.Name = "UltimosMovimientos";
+         this.Text = "Últimos Movimientos";
+         this.Click += new System.EventHandler(this.ClickEnVentana);
+         this.ResumeLayout(false);
 
         }
 
